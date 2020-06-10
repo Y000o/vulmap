a5566b6db0f60db54cc281e406d697cb55c826d6
Y000o <46235512+Y000o@users.noreply.github.com>
2020-06-09 23:26:22 -0600

    Update vulmap.sh

--- vulmap.sh
+++ vulmap.sh
@@ -16,31 +16,41 @@ fi
 clear
 echo -e " ---------------------------------------------------------------"
 echo
-echo -e " /             \                                                "
-echo -e "|    ()   ()    |                                               "
-echo -e "(_     /|\     _)                                               "
-echo -e " \_\,,,,,,,,,/_/                                                "
-echo -e "    \_'''''_/     09-06-2020 | creado por: _Y000!_   "
+echo -e "                   _        _______  _______  _______           "
+echo -e "|\     /||\     /|( \      (       )(  ___  )(  ____ )			 "
+echo -e "| )   ( || )   ( || (      | () () || (   ) || (    )| 		 "
+echo -e "| |   | || |   | || |      | || || || (___) || (____)|			 "
+echo -e "( (   ) )| |   | || |      | |(_)| ||  ___  ||  _____)			 "
+echo -e " \ \_/ / | |   | || |      | |   | || (   ) || (      			 "
+echo -e "  \   /  | (___) || (____/\| )   ( || )   ( || )      			 "
+echo -e "   \_/   (_______)(_______/|/     \||/     \||/       			 "
+echo                                             
+echo -e "                             09-06-2020 | creado por: _Y000!_   "
 echo
 echo -e "----------------------------------------------------------------"
 echo
 echo -e " [01] Escaneos dirigidos a WORDPRESS"
-echo -e " [02] Herramienta sqlmap"
+echo -e " [02] Escaneo de inyeccion xss"
 echo -e " [03] Acerca de mi"
 echo -e " [04] Salir"
 echo
-echo -n -e "nse-nmap > "
+echo -n -e "vulmap > "
 read -r vulmenu
 if [ "$vulmenu" == "01" ] || [ "$vulmenu" == "1" ];
 	then
 	clear
 	echo -e " ---------------------------------------------------------------"
 	echo
-	echo -e " /             \                                                "
-	echo -e "|    ()   ()    |                                               "
-	echo -e "(_     /|\     _)                                               "
-	echo -e " \_\,,,,,,,,,/_/                                                "
-	echo -e "    \_'''''_/      09-06-2020 | creado por: _Y000!_             "
+	echo -e "                   _        _______  _______  _______           "
+	echo -e "|\     /||\     /|( \      (       )(  ___  )(  ____ )			 "
+	echo -e "| )   ( || )   ( || (      | () () || (   ) || (    )| 		 "
+	echo -e "| |   | || |   | || |      | || || || (___) || (____)|			 "
+	echo -e "( (   ) )| |   | || |      | |(_)| ||  ___  ||  _____)			 "	
+	echo -e " \ \_/ / | |   | || |      | |   | || (   ) || (      			 "
+	echo -e "  \   /  | (___) || (____/\| )   ( || )   ( || )      			 "
+	echo -e "   \_/   (_______)(_______/|/     \||/     \||/       			 "
+	echo                                             
+	echo -e "                             09-06-2020 | creado por: _Y000!_   "
 	echo
 	echo -e "----------------------------------------------------------------"
 	echo
@@ -49,7 +59,7 @@ if [ "$vulmenu" == "01" ] || [ "$vulmenu" == "1" ];
 	echo -e " [01] Fuerza bruta de usuarios"
 	echo -e " [02] Deteccion de Temas y Plugins"
 	echo
-	echo -n -e "nse-nmap >"
+	echo -n -e "vulmap | WORDPRESS >"
 	read -r vulwordp
 		if [ "$vulwordp" == "01" ] || [ "$vulwordp" == "1" ];
 		then
@@ -67,7 +77,7 @@ if [ "$vulmenu" == "01" ] || [ "$vulmenu" == "1" ];
 		echo -n -e "Escribe la IP o la pagina: "
 		read -r iphostname
 		echo
-		nmap -sS "$iphostname"
+		nmap --script http-wordpress-enum.nse -p 80,443 "$iphostname"
 		read -rsp $'Presiona alguna tecla para continuar ...\n' -n 1 key
 		bash "$0"
 
@@ -82,12 +92,51 @@ if [ "$vulmenu" == "01" ] || [ "$vulmenu" == "1" ];
 		sleep 1
 		bash "$0"
 		fi
+
 if [ "$nsemenu" == "02" ] || [ "$nsemenu" == "2" ];
 then
-echo
-echo -e "\033[1;31m[!] Proximamente...\033[1;0m"
-sleep 1
-bash "$0"
+	clear
+	echo -e " ---------------------------------------------------------------"
+	echo
+	echo -e "                   _        _______  _______  _______           "
+	echo -e "|\     /||\     /|( \      (       )(  ___  )(  ____ )			 "
+	echo -e "| )   ( || )   ( || (      | () () || (   ) || (    )| 		 "
+	echo -e "| |   | || |   | || |      | || || || (___) || (____)|			 "
+	echo -e "( (   ) )| |   | || |      | |(_)| ||  ___  ||  _____)			 "
+	echo -e " \ \_/ / | |   | || |      | |   | || (   ) || (      			 "
+	echo -e "  \   /  | (___) || (____/\| )   ( || )   ( || )      			 "
+	echo -e "   \_/   (_______)(_______/|/     \||/     \||/       			 "
+	echo                                             
+	echo -e "                             09-06-2020 | creado por: _Y000!_   "
+	echo
+	echo -e "----------------------------------------------------------------"
+	echo 
+	echo -e " [01] Deteccion de XSS"
+	echo
+	echo -n -e "vulmap | XSS >"
+	read -r vulxss
+	if [ "$vulxss" == "01" ] || [ "$vulxss" == "1" ];
+		then
+		echo
+		echo -n -e "Escribe la IP o la pagina: "
+		read -r iphostname
+		echo
+		nmap -sV --script http-dombased-xss.nse, http-phpself-xss.nse, http-stored-xss.nse, http-xssed.nse  -p 80,443  "$iphostname"
+		read -rsp $'Presiona alguna tecla para continuar ...\n' -n 1 key
+		bash "$0"
+
+		elif [ "$vulwordp" == "00" ] || [ "$vulwordp" == "0" ];
+		then
+		echo -e "\033[1;31m[!] Atras..\033[1;0m"
+		bash "$0"
+
+		else
+		echo
+		echo -e "\033[1;31m[!] Elegiste mal...\033[1;0m"
+		sleep 1
+		bash "$0"
+		fi
+
 
 elif [ "$nsemenu" == "03" ] || [ "$nsemenu" == "3" ];
 then
